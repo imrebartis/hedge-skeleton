38d14ed81b208ea07e459e1c69ec0bf865836d08
Imre Bartis <imre.bartis@siili.com>
2017-10-25 13:45:05 +0300

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # hedge-skeleton
 
 Basic Ring Handler in Azure
+# hedge-skeleton-unit-test
--- src/my_cool_function/core.cljs
+++ src/my_cool_function/core.cljs
@@ -1,9 +1,23 @@
 (ns my-cool-function.core)
 
+(defn sum [arg1 arg2]
+  (+ arg1 arg2))
+
+(defn invert [arg]
+  (if (nil? arg) true
+  (if (false? arg) true false)))
+
+; extract query [parameter] from [req] as string
+(defn extract-query-parameter [req parameter]
+    (let [query-string (get req :query-string)]
+        (cond
+            (nil? query-string) nil
+            :else
+            (get
+                (into (sorted-map)
+                    (map (fn [x] (clojure.string/split x #"=")) (clojure.string/split (get req :query-string) #"&")))
+                parameter))))
+
 (defn crunch-my-data-function
   [req]
-  (let [string-of-commands (req :query-string)]
-
-  {:status 200,
-   :headers {"Content-Type" "application/json"},
-   :body {:commands string-of-commands, greeting "Hi?"}}))
+  )
